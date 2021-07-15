6eac1fb7074d909cf887bfa90e2cba50fbf17b8c
CN\laibin5 <laibin5>
2021-07-15 09:09:25 +0800

    add postinstall

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <img alt="Vue logo1" src="@/assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  {{xxx}}
+  <button @click="onclick">onclick</button>
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const xxx = ref(1111)
+
+const onclick = () => {
+  xxx.value = 2222
+}
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
